e1b93fe004d814d24eb638fee71c7ac561a84ede
chidubem100 <dubemernest23@gmail.com>
2023-11-17 00:11:30 +0100

    init

--- app.ts
+++ app.ts
@@ -1,10 +1,8 @@
 import fastify from 'fastify';
-import fs from 'node:fs';
-import path from 'node:path';
 
 // APP Config
 const envToLogger = {
-    development: {
+     development: {
         transport: {
             target: 'pino-pretty',
             options: {
@@ -18,20 +16,11 @@ const envToLogger = {
 }
 
 const app = fastify({
-    http2:true,
-    // https:true,
-    // https:{
-    //     key: fs.readFileSync(path.join(__dirname, '..', 'https', 'fastify.key')),
-    //     cert: fs.readFileSync(path.join(__dirname, '..', 'https', 'fastify.cert'))
-    // },
     logger: true
-})
-
-// const app = fastify({logger:envToLogger[environment] ?? true})
-
+});
 
 app.get('/health-check', async(request,reply) =>{
-    reply.code(200).send({success:true, msg: 'Health working well!!'})
+    reply.code(200).send({success:true, msg:'Health working well!!'})
 })
 
 
@@ -39,14 +28,8 @@ const port = 5000 || process.env.PORT
 
 async function startServer() {
    try {
-        await app.listen({port:port}, (err) =>{
-            if(err){
-                console.log(err)
-                process.exit(1)
-            }
-            console.log(`server started on port${port}`)
-        })
-        // console.log(`server started on port${port}`)
+        await app.listen({port:port});
+        console.log(`server started on port${port}`)
    } catch (error) {
         console.log(error)
         process.exit(1)
