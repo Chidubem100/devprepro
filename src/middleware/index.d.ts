9bf29afd99cfb9e508145e70a1515816fe6e3b57
chidubem100 <dubemernest23@gmail.com>
2023-12-27 21:38:44 +0100

    phase-1

--- src/middleware/index.d.ts
+++
@@ -1,9 +0,0 @@
-import notFound from "./notfound";
-import { errorHandler } from "./errorHandler";
-import ErrorHandler from "./MainErrorHandler";
-
-export{
-    notFound,
-    errorHandler,
-    ErrorHandler
-}
