ac092966b89fd332e0f74287dd8322f65736b99d
chidubem100 <dubemernest23@gmail.com>
2023-12-30 21:12:15 +0100

    phase-one

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import dotenv from 'dotenv';
 import {connectDB,disConnectDB} from './config/DbConnection'
 import fastifyPlugin from 'fastify-plugin';
 import { disconnect } from 'mongoose';
+import swagger from "@fastify/swagger";
+import { withRefResolver } from 'fastify-zod';
+import {version} from '../package.json'
 
 dotenv.config();
 const app = fastify();
@@ -12,11 +15,26 @@ const app = fastify();
 app.get('/health-check', async(request,reply) =>{
     console.log(request)
     return reply.code(200).send({success:true, msg:'Health working well!!'})
-})
+});
 
-app.register(
-     swa
-)
+// app.register(
+   
+// )
+
+
+// app.register(
+//    swagger,
+//    withRefResolver({
+//       routePrefix: '/docs',
+//       openapi: {
+//          info:{
+//             title: 'devprepro',
+//             description: "Api for devprepro",
+//             version,
+//          }
+//       }
+//    })
+// )
 
 const port:number = 5000 || process.env.PORT
 
