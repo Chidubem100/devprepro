8450cab8632827a661858b33e37e62388e8f33bc
chidubem100 <dubemernest23@gmail.com>
2024-03-12 01:59:56 +0100

    auth-phase3

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,17 +1,40 @@
 import { FastifyRequest,  FastifyReply} from "fastify";
 import { findUserById } from "./user.services";
-import User from "./user.model";
+// import User from "./user.model";
 import { userPayload } from "../../../typings/custom";
 
+interface UserPayload {
+    userId: string;
+    username: string;
+    email: string;
+}
+
+interface UserInterface {
+    userPayload: UserPayload
+    // refreshToken: string | undefined;
+    iat: number | undefined; 
+    exp: number | undefined
+}
+
 async function testUserHandler(request: FastifyRequest, reply: FastifyReply){
+    const x = request.user as UserInterface['userPayload'];
+    // const q =  request.user.userPayload as UserPayload
+    const y: UserInterface = {
+        userPayload: x,
+        // refreshToken: request.user.refreshToken,
+        iat: request.user.iat,
+        exp: request.user.exp
+    }
+    // cosnt x = (request.user as {userPayload: {userId: string}}).userPayload.userId;
+    // const x = (request.user as UserInterface).userPayload.userId
     // const {_id: userId} = request;
     // const user = await User.findById(req.user.userId).select('-password')
-   console.log(request.user["iat"])
-//    const x = request.user;
+   console.log(y.userPayload.userId)
+//    const x = request.user.userPayload.userId;
 //    const userId = (x as {userId:{}}).userPayload.userId;
     // const user1 = await User.findOne({_id: request.user.userId})
-    const user = await findUserById(request.user.userId)
-    reply.send(user)
+    // const user = await findUserById(x.userId)
+    // reply.send(user)
 }
 
 export {
